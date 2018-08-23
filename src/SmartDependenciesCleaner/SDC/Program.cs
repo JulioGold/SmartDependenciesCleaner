fe543d487a5e9730e1112cce08b2ef366e537acf
JulioGold <julio.gold@gmail.com>
2018-08-23 13:14:53 -0300

    Melhoria para que inclua a verificação de arquivos também.

--- src/SmartDependenciesCleaner/SDC/Program.cs
+++ src/SmartDependenciesCleaner/SDC/Program.cs
@@ -7,19 +7,28 @@ namespace SDC
     {
         static void Main(string[] args)
         {
-            string[] directories = GetDirectories(@"C:\InvalidPathToDontDoWrong", "*packages*|*node_modules*|*bin*|*obj*", SearchOption.AllDirectories);
+            string pattern = "*packages*|*node_modules*|*bin*|*obj*|package-lock.json";
+            string[] files = GetFiles(@"C:\temp\InvalidPathToDontDoWrong", pattern, SearchOption.AllDirectories);
+            string[] directories = GetDirectories(@"C:\temp\InvalidPathToDontDoWrong", pattern, SearchOption.AllDirectories);
 
-            foreach (var directory in directories)
+            foreach (var path in files.Concat(directories))
             {
-                DirectoryInfo directoryInfo = new DirectoryInfo(directory);
-                directoryInfo.Delete(true);
-            }
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
 
-            string blabla = "";
+                if (directoryInfo.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    directoryInfo.Delete(true);
+                }
+                else
+                {
+                    FileInfo fileInfo = new FileInfo(path);
+                    fileInfo.Delete();
+                }
+            }
         }
 
         /// <summary>
-        /// Faz com que seja possível especificar mais de um searchPatter separado por | pipe
+        /// Faz com que seja possível especificar mais de um searchPattern separado por | pipe
         /// </summary>
         /// <param name="path">The relative or absolute path to the directory to search. This string is not case-sensitive.</param>
         /// <param name="searchPatterns">The search string to match against the names of subdirectories in path. This parameter can contain a combination of valid literal and wildcard characters (see Remarks), but doesn't support regular expressions.</param>
@@ -28,6 +37,18 @@ namespace SDC
         private static string[] GetDirectories(string path, string searchPatterns, System.IO.SearchOption searchOption)
         {
             return searchPatterns.Split('|').SelectMany(pattern => Directory.GetDirectories(path, pattern, searchOption)).ToArray();
+        }
+
+        /// <summary>
+        /// Faz com que seja possível especificar mais de um searchPattern separado por | pipe
+        /// </summary>
+        /// <param name="path">The relative or absolute path to the directory to search. This string is not case-sensitive.</param>
+        /// <param name="searchPatterns">The search string to match against the names of subdirectories in path. This parameter can contain a combination of valid literal and wildcard characters (see Remarks), but doesn't support regular expressions.</param>
+        /// <param name="searchOption">One of the enumeration values that specifies whether the search operation should include all subdirectories or only the current directory.</param>
+        /// <returns>An array of the full names that match the specified criteria, or an empty array if no files are found.</returns>
+        private static string[] GetFiles(string path, string searchPatterns, System.IO.SearchOption searchOption)
+        {
+            return searchPatterns.Split('|').SelectMany(pattern => Directory.GetFiles(path, pattern, searchOption)).ToArray();
         }
     }
 }
